6a26fd6166f64be851b8510ad18db32281c03243
masetty <amasetty@yahoo.fr>
2021-04-18 19:58:25 +0200

    update 5th project

--- js/diaporama_projet5.js
+++ js/diaporama_projet5.js
@@ -1,26 +1,32 @@
 //creation des diaposotives
 
 var slide1 = Object.create(Slide);
-slide1.image = "pictures/projet3/projet3_1.png";
-slide1.titre = "1/7 | Accueil du site et diaporama d'explications";
+slide1.image = "pictures/projet3/projet5_1.jpg";
+slide1.titre = "1/9 | Accueil du site";
 var slide2 = Object.create(Slide);
-slide2.image = "pictures/projet3/projet3_2.png";
-slide2.titre = "2/7 | Partie réservation";
+slide2.image = "pictures/projet3/projet3_2.jpg";
+slide2.titre = "2/9 | Explication du site";
 var slide3 = Object.create(Slide);
-slide3.image = "pictures/projet3/projet3_3.png";
-slide3.titre = "3/7 | Détail d'une station";
+slide3.image = "pictures/projet3/projet3_3.jpg";
+slide3.titre = "3/9 | Explication du musée";
 var slide4 = Object.create(Slide);
-slide4.image = "pictures/projet3/projet3_4.png";
-slide4.titre = "4/7 | Signature pour confirmer la réservation";
+slide4.image = "pictures/projet3/projet3_4.jpg";
+slide4.titre = "4/9 | Se préparer : connaître son entrée";
 var slide5 = Object.create(Slide);
-slide5.image = "pictures/projet3/projet3_5.png";
-slide5.titre = "5/7 | Confirmation de la réservation et compteur";
+slide5.image = "pictures/projet3/projet3_5.jpg";
+slide5.titre = "5/9 | Accueil partie Membre";
 var slide6 = Object.create(Slide);
-slide6.image = "pictures/projet3/projet3_6.png";
-slide6.titre = "6/7 | Réservation expirée";
+slide6.image = "pictures/projet3/projet3_6.jpg";
+slide6.titre = "6/9 | Page exclusive pour les membres pour bien organiser sa visite";
 var slide7 = Object.create(Slide);
-slide7.image = "pictures/projet3/projet3_7.png";
-slide7.titre = "7/7 | Vues responsive"
+slide7.image = "pictures/projet3/projet3_7.jpg";
+slide7.titre = "7/9 | Vue d'un parcours (responsive)";
+var slide8 = Object.create(Slide);
+slide8.image = "pictures/projet3/projet3_8.jpg";
+slide8.titre = "8/9 | Accueil partie Admin"
+var slide9 = Object.create(Slide);
+slide9.image = "pictures/projet3/projet3_9.jpg";
+slide9.titre = "9/9 | Modification des entrées"
 
 //creation et actions du diaporama
 var monDiapo = Object.create(Slider);
